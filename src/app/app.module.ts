512ab4b05c32519271bc3b7085cc627c22e499e0
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-29 19:52:46 +0300

    Add Settings and ViewMode pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SettingsComponent } from './settings/settings.component';
+import { ViewModeComponent } from './view-mode/view-mode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    ViewModeComponent
   ],
   imports: [
     BrowserModule,

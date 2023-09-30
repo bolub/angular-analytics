bf46a0453243df32d253b95d6522ffaf18479fc1
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-30 13:38:50 +0300

    use dummy data

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+type GraphContent = {
+  title: string;
+  type: string;
+  color: string;
+};
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
-
+  dummyData: GraphContent[] = [
+    {
+      title: 'Chart 1',
+      type: 'chart',
+      color: 'red',
+    },
+    {
+      title: 'Chart 2',
+      type: 'bar',
+      color: 'green',
+    },
+    {
+      title: 'Chart 2',
+      type: 'line',
+      color: 'yellow',
+    },
+  ];
 }

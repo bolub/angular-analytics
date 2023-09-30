2cde0af8e1dd29b6444732956a7b264792b5460e
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-30 13:37:37 +0300

    remove unused

--- src/app/appshell/appshell.component.ts
+++ src/app/appshell/appshell.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shell',
   templateUrl: './appshell.component.html',
-  styleUrls: ['./appshell.component.css'],
 })
 export class AppshellComponent {}

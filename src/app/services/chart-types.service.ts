1ae41ed67d32a52e0639d0fe0362796cc8a95296
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-02 14:38:02 +0300

    Switch to using environment variables

--- src/app/services/chart-types.service.ts
+++ src/app/services/chart-types.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ChartType } from './mock.service';
 import { shareReplay } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +10,12 @@ import { shareReplay } from 'rxjs';
 export class ChartTypesService {
   constructor(private http: HttpClient) {}
 
-  httpUrl =
-    'https://cloud.appwrite.io/v1/databases/65182ff2ab05af399cb3/collections/65193b7ba5283eb4f7e2/documents';
+  httpUrl = environment.httpUrl;
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'X-Appwrite-Project': '65182fafbbc88f5d8f8a',
-      'X-Appwrite-Key':
-        '401fec8044f70d2a26c3ba43dc9641ff50dcb65bdb2d6c94fbe1ee5772130cebd5c4f5ceddb1f7c74620e81c42a30544c5b1d7eb76226fd52d1438d07a3dec026b44fb4118f8473be2cb1f2f3da06a0471bd1c2cc1d2a06b10a581ff4184050b1d66cec501c8dfdb615afbcf606e8d0dd662abcb7ae6988f468ce2cb39db68f1',
+      'X-Appwrite-Project': environment.projectKey,
+      'X-Appwrite-Key': environment.jwtKey,
     }),
   };
 

4ffecb534be07b146d5b36803afaf32a5c031145
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-02 15:38:06 +0300

    Implement delete

--- src/app/settings/components/graph-content/graph-content.component.ts
+++ src/app/settings/components/graph-content/graph-content.component.ts
@@ -1,10 +1,25 @@
 import { Component, Input } from '@angular/core';
-import { ChartType } from 'src/app/services/mock.service';
+import {
+  ChartTypeFull,
+  ChartTypesService,
+} from 'src/app/services/chart-types.service';
 
 @Component({
   selector: 'app-graph-content',
   templateUrl: './graph-content.component.html',
 })
 export class GraphContentComponent {
-  @Input({ required: true }) data!: ChartType;
+  @Input({ required: true }) data!: ChartTypeFull;
+
+  constructor(private chartTypesService: ChartTypesService) {}
+
+  loading = false;
+
+  onDelete() {
+    this.loading = true;
+
+    this.chartTypesService.deleteChartType(this.data.$id).subscribe((data) => {
+      this.loading = false;
+    });
+  }
 }

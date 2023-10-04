cbff24140709ff663e47540db64ea3e481f52553
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-04 03:42:49 +0300

    use a feature selector instead

--- src/app/shared/state/chart-types/chart-type.selector.ts
+++ src/app/shared/state/chart-types/chart-type.selector.ts
@@ -1,30 +1,30 @@
-import { createSelector } from '@ngrx/store';
-import { AppState } from '../app.state';
+import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { ChartTypeState } from './chart-type.reducer';
 
-export const selectChartTypes = (state: AppState) => state.chartTypes;
+export const selectChartTypesState =
+  createFeatureSelector<ChartTypeState>('chartTypes');
 
 export const selectAllChartTypes = createSelector(
-  selectChartTypes,
-  (state: ChartTypeState) => state.allChartTypes
+  selectChartTypesState,
+  (state) => state.allChartTypes
 );
 
 export const selectChartTypeStatus = createSelector(
-  selectChartTypes,
-  (state: ChartTypeState) => state.status
+  selectChartTypesState,
+  (state) => state.status
 );
 
 export const selectChartTypesLoadingStatus = createSelector(
-  selectChartTypes,
-  (state: ChartTypeState) => state.allChartTypesLoadingStatus
+  selectChartTypesState,
+  (state) => state.allChartTypesLoadingStatus
 );
 
 export const selectGraphValues = createSelector(
-  selectChartTypes,
-  (state: ChartTypeState) => state.filteredGraphValues
+  selectChartTypesState,
+  (state) => state.graphValues
 );
 
 export const selectFilteredGraphValues = createSelector(
-  selectChartTypes,
-  (state: ChartTypeState) => state.filteredGraphValues
+  selectChartTypesState,
+  (state) => state.filteredGraphValues
 );

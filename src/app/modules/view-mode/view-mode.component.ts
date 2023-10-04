b0077dd8209730011e59be4f529c52b79e9e81e8
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-04 12:32:00 +0300

    cleanup

--- src/app/modules/view-mode/view-mode.component.ts
+++ src/app/modules/view-mode/view-mode.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, combineLatest, map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { formatData } from './utils';
-import {
-  filterChartTypes,
-  loadChartTypes,
-  resetFilteredChartTypes,
-} from 'src/app/shared/state/chart-types/chart-type.action';
+import { loadChartTypes } from 'src/app/shared/state/chart-types/chart-type.action';
 import { Store } from '@ngrx/store';
 import {
   selectAllChartTypes,
   selectChartTypesLoadingStatus,
-  selectFilterRange,
-  selectFilteredGraphValues,
   selectGraphValues,
 } from 'src/app/shared/state/chart-types/chart-type.selector';
 import { ViewMode } from './view-mode.model';
@@ -25,8 +19,6 @@ export class ViewModeComponent implements OnInit {
 
   allChartTypesStatus$ = this.store.select(selectChartTypesLoadingStatus);
   graphValues$ = this.store.select(selectGraphValues);
-  filteredGraphValues$ = this.store.select(selectFilteredGraphValues);
-  filterRange$ = this.store.select(selectFilterRange);
 
   constructor(private store: Store) {}
 

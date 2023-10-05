4071d7d6984b18a883be3880df9acd9309f9c1fd
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-05 11:42:11 +0300

    remove unused

--- src/app/core/services/chart-types.service.ts
+++ src/app/core/services/chart-types.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MockService } from './mock.service';
-import { environment } from 'src/environments/environment';
 import {
   ChartType,
   ChartTypeFull,

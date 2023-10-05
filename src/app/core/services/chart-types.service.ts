15213a0b19ea677b09faf24accb58821872fb66c
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-05 11:12:10 +0300

    remove unued

--- src/app/core/services/chart-types.service.ts
+++ src/app/core/services/chart-types.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MockService } from './mock.service';
-import { Observable, shareReplay } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import {
   ChartType,

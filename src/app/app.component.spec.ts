a881398f58a7b9f45401599e7e21db811a9af027
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-08 08:02:40 +0300

    fix imports issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { AppshellComponent } from './shared/components/appshell/appshell.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent, AppshellComponent],
+      imports: [
+        RouterTestingModule,
+        AppshellComponent,
+        BrowserAnimationsModule,
+      ],
+      declarations: [AppComponent],
     })
   );
 

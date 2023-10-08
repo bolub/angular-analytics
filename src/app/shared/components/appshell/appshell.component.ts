62df25288fe24858b9a6adb44971232fc3e2107e
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-10-08 07:41:49 +0300

    Use new components

--- src/app/shared/components/appshell/appshell.component.ts
+++ src/app/shared/components/appshell/appshell.component.ts
@@ -1,7 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SideNavItemsComponent } from '../side-nav-items/side-nav-items.component';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './appshell.component.html',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    CommonModule,
+    RouterModule,
+    NavbarComponent,
+    SideNavItemsComponent,
+  ],
 })
 export class AppshellComponent {}
